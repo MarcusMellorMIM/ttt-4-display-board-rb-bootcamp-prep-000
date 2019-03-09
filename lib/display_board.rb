fbb27fa74e1878ff40f220893b14a4924313198b
Marcus Mellor <marcus@mellor.com>
2019-03-09 19:32:31 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,13 @@
-def display_board ( " "," "," "," "," ",)
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
+i=0
+y=0
+  while i < 3
+    y = i * 3
+    puts " #{board[y]} | #{board[y+1]} | #{board[y+2]} "
+    if i < 2
+      puts "-----------"
+    end
+    i = i + 1
+  end
+
+end
